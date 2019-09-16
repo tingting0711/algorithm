8a27fc1aeb02a8c976e0eba355194c1ad2674a3f
tingting0711 <twang49@buffalo.edu>
2019-09-16 16:57:14 -0400

    添加最大上升子序列模型

--- 5_dynamic_programing/895.最长上升子序列.cpp
+++ 5_dynamic_programing/895.最长上升子序列.cpp
@@ -26,10 +26,33 @@
 
 /*
 解法1：
-time : 
-space : 
+time : O(N^2)
+space : O(N)
 */
+#include<iostream>
+#include<algorithm>
+using namespace std;
+const int N = 1010;
+int n;
+int a[N], f[N];
 
+int main()
+{
+		cin>>n;
+		for(int i = 1; i <= n; i++)cin>>a[i];
+		
+		for(int i = 1; i <= n; i++)
+		{
+				f[i] = 1;
+				for(int j = 1; j < i; j++)
+						if(a[j] < a[i])f[i] = max(f[i], f[j] + 1);
+		}
+		
+		int res = 0;
+		for(int i = 1; i <= n; i++)res = max(res, f[i]);
+		cout<<res<<endl;
+		return 0;
+}
 
 
 /*
